83e5dd4731958782749544fef885d7225d12831f
Quang Nguyen <nnquang.1412@gmail.com>
2021-06-23 11:33:29 +0700

    .

--- src/components/analysis/Analysis.js
+++ src/components/analysis/Analysis.js
@@ -23,6 +23,22 @@ const Analysis = () => (
     containerId= "tradingview_17f8f"
   />
   </div> 
+  <div className="under"><TradingViewWidget
+    symbol="NASDAQ:AAPL"
+    theme={Themes.LIGHT}
+    locale="en"
+    // width="980"
+    // height="610 !important"
+    autosize
+    interval="D"
+    timezone="Etc/UTC"
+    style="1"
+    toolbarBg="#f1f3f6"
+    enablePublishing="false"
+    allowSymbolChange="true"
+    containerId= "tradingview_17f8f"
+  />
+  </div> 
   <div className="bottom"></div>
   </div>
 );
