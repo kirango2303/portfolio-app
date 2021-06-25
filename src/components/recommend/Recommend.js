76e68a9147773aa85c56897f0435910f7faf0802
Quang Nguyen <nnquang.1412@gmail.com>
2021-06-25 13:30:11 +0700

    .

--- src/components/recommend/Recommend.js
+++ src/components/recommend/Recommend.js
@@ -1,64 +1,87 @@
-import React, { useState, useRef } from "react";
-import './Recommend.css';
-import axios from "axios"
-import { Link, useHistory } from 'react-router-dom'
-import CompanyCard from '../CompanyCard/CompanyCard'
-import { Button, Form} from 'react-bootstrap';
+import React, { useState, useRef, useEffect } from "react";
+import "./Recommend.css";
+import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
+import CompanyCard from "../CompanyCard/CompanyCard";
+import { Button, Form } from "react-bootstrap";
 import CompanyFinancials from "../CompanyFinancials/CompanyFinancials";
+import { useCallbackRef } from "use-callback-ref";
+
 function Recommend() {
-    const [query, setQuery] = useState('');
-    const [similarStocks, setSimilarStocks] = useState([]);
-    const [searchedStocks, setSearchedStocks] = useState('');
-    const stocksRef = useRef()
+  const [query, setQuery] = useState("");
+  const [similarStocks, setSimilarStocks] = useState([]);
+  const [searchedStocks, setSearchedStocks] = useState("");
+  const [searching, setSearching] = useState(false);
+  const stocksRef = useCallbackRef(null, (ref) => ref && ref.focus());
+
+  const searchSimilarStocks = async () => {
     const url = `https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-recommendations?symbol=${query}`;
-    const searchSimilarStocks = async () => {
-        try {
-            const res = await axios.get(url, {
-                headers: {
-                    "x-rapidapi-key": "a48948c3dbmsh0752220de3da440p119094jsnf7916aa82678",
-                    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-                }
-            });
-
-            setSimilarStocks(res.data.finance.result[0].quotes);
-            console.log(res.data.finance.result[0].quotes);
-        } catch (err) {
-            console.log(err);
-        }
+    try {
+      const res = await axios.get(url, {
+        headers: {
+          "x-rapidapi-key":
+            "a48948c3dbmsh0752220de3da440p119094jsnf7916aa82678",
+          "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+        },
+      });
+      setSimilarStocks(res.data.finance.result[0].quotes);
+    } catch (err) {
+      console.log(err);
     }
-    console.log(similarStocks)
+  };
 
-    const handleSubmitForm = (e) => {
-        e.preventDefault()
-        searchSimilarStocks()
-        setSearchedStocks(stocksRef.current.value)
-        stocksRef.current.value=""
-  
-      }
-      console.log(searchedStocks) 
-    return (
-        <div className="body">
+  const handleSearch = () => {
+    setSearching(true);
+    setSearchedStocks(query);
+    searchSimilarStocks();
+    setQuery("");
+  };
+  const reset = () => {
+    setSearching(false);
+    setSearchedStocks("");
+    setSimilarStocks([]);
+    setQuery("");
+  };
 
-            <h1>Enter a Stocks Symbol</h1>
-            <Form onSubmit={(e) => handleSubmitForm(e)}>
-            <Form.Control className="Input" type="text" ref ={stocksRef} placeholder="i.e. AAPL"
-                    value={query} onChange={(e) => setQuery(e.target.value)} />
-                <Button className="Add-Stocks" type="submit">Find similar stocks</Button>
-            </Form>
+  return (
+    <div className="body">
+      <h1>Enter a Stocks Symbol</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="i.e. AAPL"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button onClick={handleSearch} disabled={searching}>
+          Search
+        </button>
+        <button onClick={reset}>Reset</button>
+      </div>
 
-            <div className="company-container">
-                <div>
-                {similarStocks && similarStocks.map(stock => (
-                    <CompanyCard companyName={stock.symbol} width={800} height={200}/>
-                ))}
-                </div>
-                <div>
-                   {searchedStocks && <CompanyFinancials companyName = {searchedStocks} width={500} height={800}/> }
-                </div>
-            </div>
+      <div className="company-container">
+        <div>
+          {similarStocks &&
+            similarStocks.map((stock) => (
+              <CompanyCard
+                companyName={stock.symbol}
+                width={800}
+                height={200}
+              />
+            ))}
         </div>
-    );
+        <div>
+          {searchedStocks && (
+            <CompanyFinancials
+              companyName={searchedStocks}
+              width={500}
+              height={800}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-export default Recommend
+export default Recommend;
